4a15e6a022f51014a42148f3290b74241c1e37f2
Jean Fernandine <jfernandine@gmail.com>
2022-02-23 17:23:36 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/spring/domain/PagamentoComCartao.java
+++ src/main/java/com/spring/domain/PagamentoComCartao.java
@@ -1,5 +1,34 @@
 package com.spring.domain;
 
-public class PagamentoComCartao {
+import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.spring.domain.enums.EstadoPagamento;
+
+
+@Entity
+@JsonTypeName("pagamentoComCartao")
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+		
 }
